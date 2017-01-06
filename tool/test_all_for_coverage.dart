4d0bbfde756ff627cfd444ea762b1491f4568770
Matan Lurey <matanlurey@users.noreply.github.com>
2017-01-05 22:45:36 -0500

    Run all tests when calculating coverage (#50)
    * Update travis.sh to use .analysis_options…
    
    Was accidentally pointing to a non-existing file before.
    
    * Replace uses of !<collection>isEmpty with
    
    … isNotEmpty, as-per Dart style guide.
    
    * Run all tests when calculating coverage

--- tool/test_all_for_coverage.dart
+++ tool/test_all_for_coverage.dart
@@ -1,9 +1,11 @@
-library file.tool.test_all_for_coverage;
-
 import 'package:test/test.dart';
 
+import '../test/chroot_test.dart' as chroot_test;
+import '../test/local_test.dart' as local_test;
 import '../test/memory_test.dart' as memory_test;
 
 void main() {
+  group('(chroot_test.dart)', chroot_test.main);
+  group('(local_test.dart)', local_test.main);
   group('(memory_test.dart)', memory_test.main);
 }
