c2f5b40f99822e6ca585964303be4065155210df
Todd Volkert <tvolkert@users.noreply.github.com>
2017-02-08 10:28:03 -0800

    Add deeplyEqual() method for use in replay (#25)
    Part of #11

--- lib/src/backends/record_replay/common.dart
+++ lib/src/backends/record_replay/common.dart
@@ -51,6 +51,10 @@ const String kManifestPositionalArgumentsKey = 'positionalArguments';
 /// named arguments that were passed to the method.
 const String kManifestNamedArgumentsKey = 'namedArguments';
 
+/// The key in a serialized [InvocationEvent] map that is used to store whether
+/// the invocation has been replayed already.
+const String kManifestReplayedKey = 'replayed';
+
 /// The serialized [kManifestTypeKey] for property retrievals.
 const String kGetType = 'get';
 
@@ -82,4 +86,42 @@ class TypeMatcher<T> {
 
   /// Returns `true` if the given object is of type `T`.
   bool matches(dynamic object) => object is T;
+}
+
+/// Tells whether two objects are equal using deep equality checking.
+///
+/// Two lists are deeply equal if they have the same runtime type, the same
+/// length, and every element in list A is pairwise deeply equal with the
+/// corresponding element in list B.
+///
+/// Two maps are deeply equal if they have the same runtime type, the same
+/// length, the same set of keys, and the value for every key in map A is
+/// deeply equal to the corresponding value in map B.
+///
+/// All other types of objects are deeply equal if they have the same runtime
+/// type and are logically equal (according to `operator==`).
+bool deeplyEqual(dynamic object1, dynamic object2) {
+  if (object1.runtimeType != object2.runtimeType) {
+    return false;
+  } else if (object1 is List<dynamic>) {
+    return _areListsEqual(object1, object2);
+  } else if (object1 is Map<dynamic, dynamic>) {
+    return _areMapsEqual(object1, object2);
+  } else {
+    return object1 == object2;
+  }
+}
+
+bool _areListsEqual<T>(List<T> list1, List<T> list2) {
+  int i = 0;
+  return list1.length == list2.length &&
+      list1.every((T element) => deeplyEqual(element, list2[i++]));
+}
+
+bool _areMapsEqual<K, V>(Map<K, V> map1, Map<K, V> map2) {
+  return map1.length == map2.length &&
+      map1.keys.every((K key) {
+        return map1.containsKey(key) == map2.containsKey(key) &&
+            deeplyEqual(map1[key], map2[key]);
+      });
 }
