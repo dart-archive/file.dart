d4becd76111f8ac9384f46fd75f057dcdd182655
Todd Volkert <tvolkert@users.noreply.github.com>
2017-02-12 13:39:03 -0800

    Minor fixes based on an inspection of 2.0.0 release in pub and on dartdocs.org (#37)

--- lib/chroot.dart
+++ lib/chroot.dart
@@ -2,4 +2,5 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// A file system that provides a view into _another_ [FileSystem] via a path.
 export 'src/backends/chroot.dart';
