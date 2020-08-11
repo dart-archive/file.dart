108b6cac4ceb44df6d1a133215222a545c1d6f32
jonahwilliams <jonahwilliams@google.com>
2020-08-10 17:13:33 -0700

    update CI

--- dev/bots/travis_script.sh
+++ dev/bots/travis_script.sh
@@ -11,10 +11,10 @@ elif [[ "$SHARD" == "analyze" ]]; then
   for package in "${PACKAGES[@]}"; do
     echo "Analyzing packages/$package"
     cd $ROOT/packages/$package
-    dartanalyzer --enable-experiment=non-nullable --options=$ROOT/analysis_options.yaml . || exit $?
+    dartanalyzer --options=$ROOT/analysis_options.yaml . || exit $?
   done
 else
   # tests shard
   cd $ROOT/packages/file
-  pub run --enable-experiment=non-nullable test -j1 -rexpanded || exit $?
+  pub run test -j1 -rexpanded || exit $?
 fi
